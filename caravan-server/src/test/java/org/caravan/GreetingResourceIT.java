e618251703ee1a2ecafc7809fbb16e8b509aff58
Kaarle Järvinen <kaarle46@gmail.com>
2025-01-08 10:06:09 +0200

    added shadcn

---
+++ caravan-server/src/test/java/org/caravan/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.caravan;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
