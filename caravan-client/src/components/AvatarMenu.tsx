51e8bc3c2fbed3f726b9b2ff6dae19235f23f5c5
Kaarle Järvinen <kaarle46@gmail.com>
2025-01-10 11:50:08 +0200

    Added avatar component.  Created avatarmenu to navbar with signout button. Added google as social auth provider.

---
+++ caravan-client/src/components/AvatarMenu.tsx
@@ -0,0 +1,40 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { User } from "lucide-react";
+import { signOut } from "@/auth";
+
+export default function AvatarMenu({
+  user,
+}: {
+  user?: { image?: string | null };
+}) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="rounded-full">
+        <Avatar>
+          <AvatarImage src={user?.image || ""} />
+          <AvatarFallback>
+            <User />
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem>
+          <form
+            action={async () => {
+              "use server";
+              await signOut();
+            }}
+          >
+            <button type="submit">Sign out</button>
+          </form>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- caravan-client/src/components/Navbar.tsx
+++ caravan-client/src/components/Navbar.tsx
@@ -9,11 +9,12 @@ import Link from "next/link";
 import { ModeToggle } from "./ui/mode-toggle";
 import { auth } from "@/auth";
 import Image from "next/image";
+import AvatarMenu from "./AvatarMenu";
 
 export default async function Navbar() {
   const session = await auth();
   return (
-    <NavigationMenu className="px-48 py-3 max-w-full w-full shadow-md justify-between absolute top-0 right-0 border-b backdrop-opacity-75 backdrop-blur-lg">
+    <NavigationMenu className="px-4 md:px-48 py-3 max-w-full w-full shadow-md justify-between fixed top-0 right-0 border-b backdrop-opacity-75 backdrop-blur-lg">
       <Link href="/">
         <Image
           src="/caravan-banner.png"
@@ -23,17 +24,16 @@ export default async function Navbar() {
           className="w-auto"
         />
       </Link>
-      <NavigationMenuList>
+      <NavigationMenuList className="gap-2">
         <NavigationMenuItem>
-          <Link href="/" legacyBehavior passHref>
+          <Link href="/venues" legacyBehavior passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Documentation
+              Venues
             </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
-        <ModeToggle />
         {session ? (
-          <div>ProfileMenu</div>
+          <AvatarMenu user={session.user} />
         ) : (
           <NavigationMenuItem>
             <Link href="/login" legacyBehavior passHref>
@@ -43,6 +43,7 @@ export default async function Navbar() {
             </Link>
           </NavigationMenuItem>
         )}
+        <ModeToggle />
       </NavigationMenuList>
     </NavigationMenu>
   );
