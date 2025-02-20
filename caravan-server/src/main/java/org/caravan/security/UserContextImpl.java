ba4a3d219c6dbe023bd723918ac887f2fa512f58
Kaarle Järvinen <kaarle46@gmail.com>
2025-02-20 11:44:24 +0200

    Changed to use useridentity instead of jwt

--- caravan-server/src/main/java/org/caravan/security/UserContextImpl.java
+++ caravan-server/src/main/java/org/caravan/security/UserContextImpl.java
@@ -2,9 +2,9 @@ package org.caravan.security;
 
 import org.caravan.model.User;
 import org.caravan.repository.UserRepository;
-import org.eclipse.microprofile.jwt.JsonWebToken;
 
 import io.quarkus.security.ForbiddenException;
+import io.quarkus.security.identity.SecurityIdentity;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
@@ -12,18 +12,19 @@ import jakarta.inject.Inject;
 public class UserContextImpl implements UserContext {
 
   @Inject
-   JsonWebToken jwt;
+  SecurityIdentity securityIdentity;
 
   @Inject
   UserRepository userRepository;
 
   public User getCurrentUser() {
-    String currentUserId = jwt.getSubject();
+    String currentUserId = securityIdentity.getPrincipal().getName();
+    System.out.println("currentUserId: " + currentUserId);
     return userRepository.findById(currentUserId);
   }
 
   public void validateOwnerShip(String resourceOwnerId) {
-    String currentUserId = jwt.getSubject();
+    String currentUserId = securityIdentity.getPrincipal().getName();
     if (!currentUserId.equals(resourceOwnerId)) {
       throw new ForbiddenException("You do not own this resource.");
     }
