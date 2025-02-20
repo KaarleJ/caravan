69ab78042630b9be56db0917e34185045c33f05f
Kaarle Järvinen <kaarle46@gmail.com>
2025-02-20 10:49:10 +0200

    Removed redundant custom exceptions

--- caravan-server/src/main/java/org/caravan/exception/InvalidClientSecretException.java
+++
@@ -1,7 +0,0 @@
-package org.caravan.exception;
-
-public class InvalidClientSecretException extends RuntimeException {
-  public InvalidClientSecretException(String message) {
-    super(message);
-  }
-}
--- caravan-server/src/main/java/org/caravan/mapper/GlobalExceptionMapper.java
+++ caravan-server/src/main/java/org/caravan/mapper/GlobalExceptionMapper.java
@@ -1,9 +1,6 @@
 package org.caravan.mapper;
 
 import jakarta.ws.rs.core.Response;
-
-import org.caravan.exception.InvalidClientSecretException;
-import org.caravan.exception.InvalidCredentialsException;
 import org.caravan.exception.UserNotFoundException;
 import org.jboss.resteasy.reactive.RestResponse;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
@@ -15,23 +12,7 @@ public class GlobalExceptionMapper {
    */
   @ServerExceptionMapper
   public RestResponse<String> handleUserNotFound(UserNotFoundException e) {
-    return RestResponse.status(Response.Status.NOT_FOUND, e.getMessage());
-  }
-
-  /*
-   * Returns HTTP 401 for an InvalidCredentialsException
-   */
-  @ServerExceptionMapper
-  public RestResponse<String> handleInvalidPassword(InvalidCredentialsException e) {
-    return RestResponse.status(Response.Status.UNAUTHORIZED, e.getMessage());
-  }
-
-  /*
-   * Returns HTTP 401 for an InvalidClientSecretException
-   */
-  @ServerExceptionMapper
-  public RestResponse<String> handleInvalidClientSecret(InvalidClientSecretException e) {
-    return RestResponse.status(Response.Status.UNAUTHORIZED, e.getMessage());
+    return RestResponse.status(Response.Status.NOT_FOUND, "{\"message\": \"User not found\"}");
   }
 
   /*
